47522e4db279ca6edfeec80f6568e0e0587e5a52
fubira <fubira@gmail.com>
2017-05-05 01:14:49 +0900

    Automatically change a camera setting during a flight

--- src/main/java/net/ironingot/flightview/FlightView.java
+++ src/main/java/net/ironingot/flightview/FlightView.java
@@ -1,5 +1,7 @@
 package net.ironingot.flightview;
 
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.fml.common.Mod;
@@ -7,9 +9,16 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.Style;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
 
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
@@ -25,17 +34,19 @@ public class FlightView
 {
     private static final Logger logger = FMLLog.getLogger();
     public static final KeyBinding keybinding = new KeyBinding("Toggle FlightView Mode", Keyboard.KEY_V, "FlightView");
-    private static boolean isActive = false;
+    private static boolean is_active = false;
 
     @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
+    public void preInit(FMLPreInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(this);
+        new WorldRenderLastEventListener();
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
         ClientRegistry.registerKeyBinding(keybinding);
+        logger.info("[FlightView] Initialized.");
     }
 
     @SideOnly(Side.CLIENT)
@@ -59,13 +70,23 @@ public class FlightView
         }
     }
 
+    public static void message(String s)
+    {
+        Minecraft.getMinecraft().player.sendMessage(new TextComponentString("")
+            .appendSibling((new TextComponentString("[")).setStyle((new Style()).setColor(TextFormatting.GRAY)))
+            .appendSibling((new TextComponentString("FlightView")).setStyle((new Style()).setColor(TextFormatting.GREEN)))
+            .appendSibling((new TextComponentString("] ")).setStyle((new Style()).setColor(TextFormatting.GRAY)))
+            .appendSibling((new TextComponentString(s))));
+    }
+
     public static boolean isActive()
     {
-        return isActive;
+        return is_active;
     }
 
     public static void toggle()
     {
-        isActive = isActive ? false : true;
+        is_active =is_active ? false : true;
+        message("FlightView " + ((is_active) ? "is Actived." : "is Inactived."));
     }
 }
---
+++ src/main/java/net/ironingot/flightview/WorldRenderLastEventListener.java
@@ -0,0 +1,86 @@
+package net.ironingot.flightview;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.client.event.RenderWorldLastEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.common.FMLLog;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
+import org.apache.logging.log4j.Logger;
+
+public class WorldRenderLastEventListener
+{
+    private static final Logger logger = FMLLog.getLogger();
+    private boolean isLastFlying = false;
+    private long timeFlyingStart = 0;
+
+    private int lastViewState = 0;
+    private boolean isViewChanged = false;
+
+    public WorldRenderLastEventListener()
+    {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @SubscribeEvent
+    public void onWorldRenderLast(RenderWorldLastEvent event)
+    {
+        EntityPlayerSP player = Minecraft.getMinecraft().player;
+
+        if (!FlightView.isActive())
+            return;
+
+        if (!isLastFlying && player.isElytraFlying())
+        {
+            startElytraFlying();
+            timeFlyingStart = Minecraft.getSystemTime();
+            isLastFlying = true;
+        }
+
+        if (isLastFlying && !player.isElytraFlying())
+        {
+            endElytraFlying();
+            isLastFlying = false;
+        }
+
+        if (isLastFlying && player.isElytraFlying())
+        {
+            updateElytraFlying(Minecraft.getSystemTime() - timeFlyingStart);
+        }
+    }
+
+    public void startElytraFlying()
+    {
+        lastViewState = Minecraft.getMinecraft().gameSettings.thirdPersonView;
+        isViewChanged = false;
+    }
+
+    public void endElytraFlying()
+    {
+        Minecraft.getMinecraft().gameSettings.thirdPersonView = lastViewState;
+    }
+
+    public void updateElytraFlying(long timeFlying)
+    {
+        if (!isViewChanged && timeFlying > 1000)
+        {
+            Minecraft.getMinecraft().gameSettings.thirdPersonView = 1;
+            isViewChanged = true;
+        }
+    }
+}
