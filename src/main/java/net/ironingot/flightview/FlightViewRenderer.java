a324afe668e5d81ddaec400d3a4d345a3f04fc40
fubira <fubira@gmail.com>
2017-05-05 22:01:44 +0900

    Update layout

--- src/main/java/net/ironingot/flightview/FlightViewRenderer.java
+++ src/main/java/net/ironingot/flightview/FlightViewRenderer.java
@@ -1,5 +1,8 @@
 package net.ironingot.flightview;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
@@ -14,6 +17,7 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.RenderItem;
@@ -29,8 +33,6 @@ import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentString;
 
-import org.lwjgl.input.Keyboard;
-import org.lwjgl.input.Mouse;
 import org.apache.logging.log4j.Logger;
 
 public class FlightViewRenderer
@@ -55,15 +57,83 @@ public class FlightViewRenderer
             ItemStack itemstack = player.getItemStackFromSlot(EntityEquipmentSlot.CHEST);
             GuiScreen screen = Minecraft.getMinecraft().currentScreen;
 
-            if (itemstack.getItem() == Items.ELYTRA)
-                renderElytra(16, 16, player, itemstack);
+            renderFlightInfo(8, 32, player, itemstack);
         }
     }
 
-    protected void renderElytra(int x, int y, EntityPlayer player, ItemStack stack)
+    protected List<String> getFlightInfoString(EntityPlayer player)
+    {
+        List<String> stringArray = new ArrayList<String>();
+        double dx = player.posX - player.prevPosX;
+        double dy = player.posY - player.prevPosY;
+        double dz = player.posZ - player.prevPosZ;
+        double groundSpeed = Math.sqrt(dx * dx + dz * dz);
+        double airSpeed = Math.sqrt(dx * dx + dy * dy + dz * dz);
+        double yaw = player.rotationYaw;
+        double pitch = player.rotationPitch;
+
+        while(yaw < 0D) yaw += 360D;
+        while(yaw > 360D) yaw -= 360D;
+
+        stringArray.add(String.format("GS: %.2f(km/h)", groundSpeed * 20.0D * 3.6D));
+        stringArray.add(String.format("AS: %.2f(km/h)", airSpeed * 20.0D * 3.6D));
+        stringArray.add(String.format("Y: %.1f P: %.1f", yaw, pitch));
+
+        return stringArray;
+    }
+
+    protected int getElytraLife(ItemStack stack)
+    {
+        return stack.getMaxDamage() - stack.getItemDamage();
+    }
+
+    protected String getElytraInfoString(ItemStack stack)
+    {
+        return String.format("%3d", getElytraLife(stack));
+    }
+
+    protected int getElytraInfoColor(ItemStack stack)
+    {
+        int color = 0xffffff;
+        float left = stack.getItemDamage() / stack.getMaxDamage();
+        if (left > 0.95D) color = 0xff0000;
+        else if (left > 0.9D) color = 0xff8000;
+        return color;
+    }
+
+    protected int renderFlightInfo(int x, int y, EntityPlayer player, ItemStack stack)
     {
         Minecraft mc = Minecraft.getMinecraft();
-        RenderItem itemRenderer = Minecraft.getMinecraft().getRenderItem();
+        List<String> flightInfoString = getFlightInfoString(player);
+        String elytraInfoString = getElytraInfoString(stack);
+
+        final int lineHeight = 10;
+        int height = 0;
+        int width = 0;
+
+        for (int i = 0; i < flightInfoString.size(); i++) {
+            width = Math.max(mc.fontRendererObj.getStringWidth(flightInfoString.get(i)), width);
+        }
+        height = flightInfoString.size() * lineHeight;
+        height += lineHeight * 1.6;
+
+        Gui.drawRect(x - 2, y - 2, x + width + 2, y + height + 2, 0x60000000);
+        for (int i = 0; i < flightInfoString.size(); i++) {
+            mc.fontRendererObj.drawStringWithShadow(flightInfoString.get(i), x, y + lineHeight * i, 0xffffff);
+        }
+
+        if (stack.getItem() == Items.ELYTRA) {
+            mc.fontRendererObj.drawStringWithShadow(elytraInfoString, x + 20, y + flightInfoString.size() * lineHeight + lineHeight * 0.5f, 0xffffff);
+            drawElytraIcon(x, y + flightInfoString.size() * lineHeight, stack);
+        }
+
+        return height;
+    }
+
+    protected void drawElytraIcon(int x, int y, ItemStack stack)
+    {
+        Minecraft mc = Minecraft.getMinecraft();
+        RenderItem itemRenderer = mc.getRenderItem();
 
         GlStateManager.pushMatrix();
         GlStateManager.enableRescaleNormal();
@@ -71,42 +141,12 @@ public class FlightViewRenderer
         GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
         RenderHelper.enableGUIStandardItemLighting();
 
-        itemRenderer.renderItemAndEffectIntoGUI(player, stack, x, y + 32);
-        itemRenderer.renderItemOverlays(mc.fontRendererObj, stack, x, y + 32);
+        itemRenderer.renderItemAndEffectIntoGUI(stack, x, y);
+        itemRenderer.renderItemOverlayIntoGUI(mc.fontRendererObj, stack, x, y, null);
 
         GlStateManager.popMatrix();
         GlStateManager.disableRescaleNormal();
         GlStateManager.disableBlend();
         RenderHelper.disableStandardItemLighting();
-
-        int life = stack.getMaxDamage() - stack.getItemDamage();
-        String s = "" + life;
-        int color = (life < 30) ? 0xff0000 : 0xffffff;
-        mc.fontRendererObj.drawStringWithShadow(s, x + 18, y + 35 + 4, color);
-
-        Vec3d vec3d = player.getLookVec();
-        double direction = Math.sqrt(vec3d.xCoord * vec3d.xCoord + vec3d.zCoord * vec3d.zCoord);
-
-        double dx = player.posX - player.prevPosX;
-        double dy = player.posY - player.prevPosY;
-        double dz = player.posZ - player.prevPosZ;
-
-        double groundSpeed = Math.sqrt(dx * dx + dz * dz);
-        s = String.format("GS: %4.2f(km/h)", groundSpeed * 20.0D * 3.6D);
-        mc.fontRendererObj.drawStringWithShadow(s, x + 0, y + 2, 0xffffff);
-
-        double airSpeed = Math.sqrt(dx * dx + dy * dy + dz * dz);
-        s = String.format("AS: %4.2f(km/h)", airSpeed * 20.0D * 3.6D);
-        mc.fontRendererObj.drawStringWithShadow(s, x + 0, y + 13, 0xffffff);
-
-        double yaw = player.rotationYaw;
-        while(yaw < 0D) yaw += 360D;
-        while(yaw > 360D) yaw -= 360D;
-        yaw -= 180;
-
-        double pitch = player.rotationPitch;
-        s = String.format("Y: %-3.1f P: %.1f", yaw, pitch);
-        mc.fontRendererObj.drawStringWithShadow(s, x + 0, y + 24, 0xffffff);
     }
-
 }
