410cb6af3f4a71a602937300bb705116a7e74d30
fubira <fubira@gmail.com>
2022-06-21 18:09:51 +0900

    v1.3

--- forge/src/main/java/net/ironingot/flightview/forge/FlightViewMod.java
+++ forge/src/main/java/net/ironingot/flightview/forge/FlightViewMod.java
@@ -73,7 +73,6 @@ public class FlightViewMod {
 
     public static void message(String s) {
         Minecraft mc = Minecraft.getInstance();
-        // mc.player.sendMessage(
         mc.player.sendSystemMessage(
             Component.Serializer.fromJson("[\"\",{\"text\":\"[\",\"color\":\"gray\"},{\"text\":\"FlightView\",\"color\":\"dark_green\"},{\"text\":\"]\",\"color\":\"gray\"},{\"text\":\" " + s + "\"}]")
         );
